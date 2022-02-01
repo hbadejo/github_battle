6668e9ab65320672a2b7365c81cd676d84048c31
Abdulhakeem BADEJO <32986101+hbadejo@users.noreply.github.com>
2022-02-01 23:13:52 +0100

    Update scriptP2.js

--- scriptP2.js
+++ scriptP2.js
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function gotoPTab() {
         setTimeout(() => {
             location.reload();
-            const playerTabUrl = '/playersTable.html';
+            const playerTabUrl = 'https://hbadejo.github.io/playersTable.html';
             window.location = playerTabUrl;
         }, 2000);      
     };
