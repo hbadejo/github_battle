b046593aa431617d2c9a777e48fe1149eff0bbaa
Abdulhakeem BADEJO <32986101+hbadejo@users.noreply.github.com>
2022-02-08 12:20:50 +0100

    Update scriptP1.js

--- scriptP1.js
+++ scriptP1.js
@@ -9,10 +9,8 @@ document.addEventListener('DOMContentLoaded', function() {
     varDec.getP1Data.addEventListener('click', function (e) {
               
         // check if user has entered a value into the input box
-        if (varDec.player1Name.value === '') {
-            return false;
-        }else {
-                 // Promise for fetching data from github API
+        if (varDec.player1Name.value !== '') {
+                     // Promise for fetching data from github API
         P1Data = new URL (`https://api.github.com/users/${varDec.player1Name.value}`);
         const playerOne = fetch(P1Data).then(function (respone) {
             return respone.json();
